83ee8bd466f71fde00b321cfb5a27e87385db48e
haiduk <20hwlee@gmail.com>
2023-05-14 02:17:11 -0400

    hamburger icon functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
+import NavBar from "@/components/NavBar"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,36 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>
-          <div className="justify-between w-screen bg-black h-16 inline-flex px-6 py-auto fixed font-medium top-0">
-            <div>
-              <h3 className="text-2xl font-medium text-blue-500 m-4">
-                EXAMPLE
-              </h3>
-            </div>
-            <div className="hidden pr-2 space-x-8 lg:flex">
-              <a href="#" className="text-white m-4">
-                About Us
-              </a>
-              <a href="#" className="text-white m-4">
-                Our Work
-              </a>
-              <a href="#" className="text-white m-4">
-                Our Partners
-              </a>
-              <a href="#" className="text-white m-4">
-                Contact Us
-              </a>
-            </div>
-          </div>
-          <div className="flex m-4 p-1 justify-end lg:hidden">
-            <div className="space-y-2">
-              <span className="block w-8 h-0.5 bg-white animate-pulse"></span>
-              <span className="block w-8 h-0.5 bg-white animate-pulse"></span>
-              <span className="block w-8 h-0.5 bg-white animate-pulse"></span>
-            </div>
-          </div>
-        </nav>
+        <NavBar />
         {/* <nav className="container flex justify-between px-4 py-8 mx-auto bg-white">
           <div>
             <h3 className="text-2xl font-medium text-blue-500">LOGO</h3>
---
+++ components/NavBar.tsx
@@ -0,0 +1,108 @@
+"use client"
+import * as React from "react"
+
+export default function NavBar() {
+  const [isNavOpen, setIsNavOpen] = React.useState(false)
+  return (
+    <nav>
+      <div className="justify-between w-screen bg-black h-16 inline-flex px-6 py-auto fixed font-medium top-0">
+        <div>
+          <h3 className="text-2xl font-medium text-blue-500 m-4">EXAMPLE</h3>
+        </div>
+        <div className="hidden pr-2 space-x-8 lg:flex">
+          <a
+            href="#"
+            className="hover:animate-pulse uppercase text-white m-4 border-b border-gray-400"
+          >
+            About Us
+          </a>
+          <a
+            href="#"
+            className="hover:animate-pulse uppercase text-white m-4 border-b border-gray-400"
+          >
+            Our Work
+          </a>
+          <a
+            href="#"
+            className="hover:animate-pulse uppercase text-white m-4 border-b border-gray-400"
+          >
+            Our Partners
+          </a>
+          <a
+            href="#"
+            className="hover:animate-pulse uppercase text-white m-4 border-b border-gray-400"
+          >
+            Contact Us
+          </a>
+        </div>
+      </div>
+      <div className="flex m-4 p-1 justify-end lg:hidden">
+        <div
+          className="space-y-2"
+          onClick={() => setIsNavOpen((prev) => !prev)}
+        >
+          <span className="block w-8 h-0.5 bg-white animate-pulse"></span>
+          <span className="block w-8 h-0.5 bg-white animate-pulse"></span>
+          <span className="block w-8 h-0.5 bg-white animate-pulse"></span>
+        </div>
+        <div
+          className={isNavOpen ? "showMenuNav" : "hideMenuNav"}
+          onClick={() => setIsNavOpen(false)}
+        >
+          <div className="CROSS-ICON absolute top-0 right-0 px-8 py-8">
+            <svg
+              className="h-8 w-8 text-gray-600"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </div>
+          <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px] ">
+            <li className="border-b border-gray-400 my-8 uppercase">
+              <a
+                href="#"
+                className="text-white m-4"
+                onClick={() => setIsNavOpen(false)}
+              >
+                About Us
+              </a>
+            </li>
+            <li className="border-b border-gray-400 my-8 uppercase">
+              <a
+                href="#"
+                className="text-white m-4"
+                onClick={() => setIsNavOpen(false)}
+              >
+                Our Work
+              </a>
+            </li>
+            <li className="border-b border-gray-400 my-8 uppercase">
+              <a
+                href="#"
+                className="text-white m-4"
+                onClick={() => setIsNavOpen(false)}
+              >
+                Our Partners
+              </a>
+            </li>
+            <li className="border-b border-gray-400 my-8 uppercase">
+              <a
+                href="#"
+                className="text-white m-4"
+                onClick={() => setIsNavOpen(false)}
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
