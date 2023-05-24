d42a97f297701a97dff44b1a759441dd078dd4c7
haiduk <20hwlee@gmail.com>
2023-05-24 02:55:27 -0400

    observer animation for hero

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,10 +1,19 @@
 "use client"
 import * as React from "react"
+import { useInView } from "react-intersection-observer"
 
 export default function HeroBanner() {
+  const { ref, inView, entry } = useInView({
+    threshold: 0.4,
+    // triggerOnce: true,
+  })
+
   return (
     <>
-      <div className="w-screen h-screen bg-black border-white text-white mt-26">
+      <div
+        ref={ref}
+        className="w-screen h-screen bg-black border-white text-white mt-26"
+      >
         <div className="float-left bg-black w-2/5 h-full z-20">
           <div className="h-1/5 md:h-1/3 lg:2/5"></div>
           <div className="px-3 md:px-6 lg:px-12 heroText">
@@ -16,16 +25,22 @@ export default function HeroBanner() {
           </div>
         </div>
         <div
-          className="heroBlocks float-left bg-yellow-500 w-1/5 h-full"
-          style={{ animationDelay: "250ms" }}
+          className={`${
+            inView ? "heroBlocks1 opacity-1" : "opacity-0"
+          } float-left bg-yellow-500 w-1/5 h-full`}
+          // style={{ animationDelay: "250ms" }}
         ></div>
         <div
-          className="heroBlocks float-left bg-orange-600 w-1/5 h-full"
-          style={{ animationDelay: "500ms" }}
+          className={`${
+            inView ? "heroBlocks2 opacity-1" : "opacity-0"
+          } float-left bg-orange-600 w-1/5 h-full`}
+          // style={{ animationDelay: "500ms" }}
         ></div>
         <div
-          className="heroBlocks float-left bg-amber-800 w-1/5 h-full"
-          style={{ animationDelay: "750ms" }}
+          className={`${
+            inView ? "heroBlocks3 opacity-1" : "opacity-0"
+          } float-left bg-amber-800 w-1/5 h-full`}
+          // style={{ animationDelay: "750ms" }}
         ></div>
       </div>
     </>
